f589b31dd7fbd38dea86e9009cc84d0a3db7ec24
Anurag Gupta <guptaanurag1610@gmail.com>
2020-12-19 13:11:30 +0530

    url changes3

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,7 +1,4 @@
-const baseUrl =
-  location.hostname === "localhost"
-    ? "http://localhost:2000"
-    : "https://rest-server12.herokuapp.com/";
+const baseUrl = process.env.API || "https://rest-server12.herokuapp.com";
 //const baseUrl = "http://localhost:2000";
 
 export const api = `${baseUrl}/api`;
